70a1f2855911e01b496ac3c6f45b092d93e9d2f7
ljzsdut <ljzsdut@163.com>
2019-11-07 10:37:44 +0800

    增加deploy.sh\compton.conf

--- deploy.sh
+++ deploy.sh
@@ -2,26 +2,29 @@
 prefix_dir=$(cd "$(dirname "$0")"; pwd)   #获取脚本的绝对路径
 
 #vim配置文件
-[[ -e "~/.vimrc" ]] && mv -f ~/.vimrc{,.bak}
+[[ -e ~/.vimrc ]] && mv -f ~/.vimrc{,.bak}
 ln -s ${prefix_dir}/vimrc ~/.vimrc
 
-#neovim配置文件
-[[ -e "~/.config/nvim/init.vim" ]] && mv -f ~/.config/nvim/init.vim{,.bak}
-ln -s ${prefix_dir}/vimrc ~/.config/nvim/init.vim
+##neovim配置文件
+#[[ -d ~/.config/nvim/autoload ]] || mkdir ~/.config/nvim/autoload
+#[[ -e ~/.config/nvim/init.vim ]] && mv -f ~/.config/nvim/init.vim{,.bak}
+#ln -s ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/plug.vim
+#ln -s ${prefix_dir}/vimrc ~/.config/nvim/init.vim
 
 #alacritty配置文件
+[[ -e ~/.config/alacritty/alacritty.yml ]] && mv -f ~/.config/alacritty/alacritty.yml{,.bak}
 ln -s ${prefix_dir}/alacritty.yml  ~/.config/alacritty/alacritty.yml
 
 #albert
-[[ -e "~/.config/albert/albert_run" ]] && mv -f ~/.config/albert/albert_run{,.bak}
+[[ -e ~/.config/albert/albert_run ]] && mv -f ~/.config/albert/albert_run{,.bak}
 ln -s ${prefix_dir}/albert_run ~/.config/albert/albert_run
 
 #i3
-[[ -e "~/.config/i3" ]] && mv -f ~/.config/i3{,.bak}
+[[ -e ~/.config/i3 ]] && mv -f ~/.config/i3{,.bak}
 ln -s ${prefix_dir}/i3 ~/.config/i3
 
-##compton,需要编译安装:
-#[[ -e "~/.config/compton.conf" ]] && mv -f ~/.config/compton.conf{,.bak}
+##compton,需要编译安装:https://github.com/ljzsdut/compton
+#[[ -e ~/.config/compton.conf ]] && mv -f ~/.config/compton.conf{,.bak}
 #ln -s ${prefix_dir}/compton.conf ~/.config/compton.conf
 
 
